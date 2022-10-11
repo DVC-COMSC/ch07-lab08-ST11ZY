ebeb265cd1a1c73361e5d98c9e83b28fd7f0c006
Your Name <you@example.com>
2022-10-11 00:05:15 -0700

    code

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # ******************************
 # Make your Code
 # ******************************
+inputvalues=input('Enter all elements values in acending order:')
+numbers1=inputvalues.split()
+inputvalue=int(input("Value"))
+i=0
+while i <len(numbers1):
+    numbers1[i]=int(numbers1[i])
+    if int(inputvalue)>int(numbers1[i-1]) and int(inputvalue)<int(numbers1[i]):
+        numbers1.insert(i,inputvalue)
+    i=i+1
+print(numbers1)
+        
