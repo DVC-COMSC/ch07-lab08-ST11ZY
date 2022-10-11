8e1a6cb9573cd44bcba060023aa7eaefc5ed42b8
Your Name <you@example.com>
2022-10-11 00:23:47 -0700

    code

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ inputvalue=int(input("Value"))
 i=0
 while i <len(numbers1):
     numbers1[i]=int(numbers1[i])
-    if int(inputvalue)>=int(numbers1[i-1]) and int(inputvalue)<int(numbers1[i]):
+    if int(inputvalue)>int(numbers1[i-1]) and int(inputvalue)<int(numbers1[i]):
         numbers1.insert(i,inputvalue)
     i=i+1
 print(numbers1)
